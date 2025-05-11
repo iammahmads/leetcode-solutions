14af2ff919959a7b0f96b257d494fd633833388d
Muhammad Ahmad Shahid <mahmads0323@gmail.com>
2025-05-11 17:09:58 +0500

    Problem: threeConsectiveOdds

---
+++ threeConsectiveOdds.py
@@ -0,0 +1,18 @@
+class Solution:
+    def threeConsecutiveOdds(self, arr):
+        """
+        :type arr: List[int]
+        :rtype: bool
+        """
+        consective_odds = 0
+        for i in range(len(arr)):
+            if(arr[i] % 2 != 0):
+                consective_odds = consective_odds + 1
+                if consective_odds == 3:
+                    return True
+            else:
+                consective_odds = 0
+        return False
+    
+sol = Solution()
+print(sol.threeConsecutiveOdds([1, 4, 5, 8, 9]))
